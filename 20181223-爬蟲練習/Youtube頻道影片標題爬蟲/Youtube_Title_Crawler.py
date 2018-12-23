97d1fa2b5dd7602de3489c438441fb1b8375f8e1
Yanwei Liu <e96031413@hotmail.com>
2018-12-23 17:04:01 +0800

    added

--- 20181223-爬蟲練習/Youtube頻道影片標題爬蟲/Youtube_Title_Crawler.py
+++ 20181223-爬蟲練習/Youtube頻道影片標題爬蟲/Youtube_Title_Crawler.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
-url = 'https://www.youtube.com/channel/UCFdTiwvDjyc62DBWrlYDtlQ/videos'
+url = 'https://www.youtube.com/channel/UCFdTiwvDjyc62DBWrlYDtlQ/videos?sort=p&view=0&flow=grid'
 resp = requests.get(url,headers=headers)
 soup = BeautifulSoup(resp.text, 'lxml')
 
 target=soup.find_all('a')
+txt = open('video-title.txt', 'w', encoding = 'UTF-8')
 for i in target:
-	print(i.get_text().strip())
+	f=i.get_text().strip()
+	txt.write(f)
+txt.close()
+
+
